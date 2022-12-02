84c4992ed3babcde09fbc5430aa1fb979a453471
mzhai-UofC <37128021+mzhai-UofC@users.noreply.github.com>
2022-12-01 19:52:40 -0700

    Add files via upload

---
+++ Unreal_Shooter_Game_Project/Source/Shooter/Weapon.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SHOOTER_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+public:
+	AWeapon();
+
+	virtual void Tick(float DeltaTime) override;
+protected:
+	void StopFalling();
+private:
+	FTimerHandle ThrowWeaponTimer;
+	float ThrowWeaponTime;
+	bool bFalling;
+public:
+	/** Adds an impulse to the Weapon */
+	void ThrowWeapon();
+};
